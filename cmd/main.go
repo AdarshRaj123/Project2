9da6cf6e6eff8fb863ed247228fd602c98d5dbc0
AdarshRaj123 <adarsh.srivastava@remotestate.com>
2022-01-27 13:59:24 +0530

    fix/some-bugs

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"Project1/utils"
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/go-chi/chi/v5"
 	"io/ioutil"
 	"net/http"
 	"time"
@@ -17,31 +19,43 @@ type WordCount struct {
 type WordCountList []WordCount
 
 func main() {
-	text := struct {
-		Text string `json:"text"`
-	}{Text: "he is a good boy very good boy he loves music and loves to be done not learning to go out"}
-	textjson, err := json.Marshal(text)
-	req, err := http.NewRequest("POST", "http://localhost:3000/text", bytes.NewBuffer(textjson))
-	if err != nil {
-		fmt.Printf("%s", err)
-		return
-	}
-	req.Header.Add("Accept", `application/json`)
+	router := chi.NewRouter()
+	router.Post("/text", func(writer http.ResponseWriter, request *http.Request) {
+		jdata := struct {
+			Text string `json :"text"`
+		}{}
 
-	c := http.Client{Timeout: time.Duration(1) * time.Second}
-	resp, err := c.Do(req)
-	if err != nil {
-		fmt.Printf("Error %s", err)
-		return
+		if parseErr := utils.ParseBody(request.Body, &jdata); parseErr != nil {
+			utils.RespondError(writer, http.StatusBadRequest, parseErr, "failed to parse request body")
+			return
+		}
 
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	p := make(WordCountList, 10)
-	err1 := json.Unmarshal(body, &p)
-	if err1 != nil {
+		textjson, err := json.Marshal(jdata)
+		fmt.Println(textjson)
+		req, err := http.NewRequest("POST", "http://localhost:3000/text", bytes.NewBuffer(textjson))
+		if err != nil {
+			fmt.Printf("%s", err)
+			return
+		}
+		req.Header.Add("Accept", `application/json`)
 
-	}
-	fmt.Println("Body : %s \n ", p)
+		c := http.Client{Timeout: time.Duration(1) * time.Second}
+		resp, err := c.Do(req)
+		if err != nil {
+			fmt.Printf("Error %s", err)
+			return
+
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		p := make(WordCountList, 10)
+		err1 := json.Unmarshal(body, &p)
+		if err1 != nil {
+
+		}
+		utils.RespondJSON(writer, http.StatusOK, p[0:11])
+
+	})
+	http.ListenAndServe(":8080", router)
 
 }
---
+++ go.mod
@@ -0,0 +1,20 @@
+module Project1
+
+go 1.17
+
+require (
+	github.com/go-chi/chi/v5 v5.0.5
+	github.com/go-playground/validator/v10 v10.9.0
+	github.com/sirupsen/logrus v1.8.1
+	github.com/teris-io/shortid v0.0.0-20201117134242-e59966efd125
+)
+
+require (
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
+	golang.org/x/text v0.3.7 // indirect
+
+)
